358e1079f40a8784b26c21e8fa1d9832677c005f
claypot-ai <imclaypot@gmail.com>
2023-01-25 17:34:21 +0800

    Refactor

--- simple-webapp/backend/server.js
+++ simple-webapp/backend/server.js
@@ -1,9 +1,10 @@
 const http = require("http");
 const url = require("url");
-const greeting = require("name");
+const greet = require("./greet");
 
 const server = http.createServer((req, res) => {
     const { name } = url.parse(req.url, true).query;
+    const greeting = greet(name);
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.setHeader("Access-Control-Allow-Origin", "*");
